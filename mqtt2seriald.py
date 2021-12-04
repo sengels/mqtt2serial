104c596062c36231575598d3ebf5f6049aea9f2f
Patrick Spendrin <ps_ml@gmx.de>
2021-12-04 22:09:52 +0100

    update to a proper DaemonContext

--- mqtt2seriald.py
+++ mqtt2seriald.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-from daemon.runner import DaemonRunner
+from daemon import DaemonContext
+from lockfile import FileLock
 import time
 import logging
 from mqtt2serial import main
 
-class MqttDaemonRunner(DaemonRunner):
-    def _open_streams_from_app_stream_paths(self, app):
-        """ Open the `daemon_context` streams from the paths specified.
-    
-            :param app: The application instance.
-    
-            Open the `daemon_context` standard streams (`stdin`,
-            `stdout`, `stderr`) as stream objects of the appropriate
-            types, from each of the corresponding filesystem paths
-            from the `app`.
-            """
-        self.daemon_context.stdin = open(app.stdin_path, 'rt')
-        self.daemon_context.stdout = open(app.stdout_path, 'w+t')
-        self.daemon_context.stderr = open(app.stderr_path, 'w+t')
-
 class MqttApp(object):
     def __init__(self):
-        self.stdout_path = '/dev/null'
-        self.stderr_path = '/dev/null'
-        self.stdin_path = '/dev/null'
         self.pidfile_path = '/var/run/mqtt2seriald.pid'
         self.pidfile_timeout = 10
 
@@ -40,5 +23,6 @@ class MqttApp(object):
         except:
             logging.exception("some exception")
 
-run = MqttDaemonRunner(MqttApp())
-run.do_action()
+app = MqttApp()
+with DaemonContext(pidfile=FileLock(app.pidfile_path, app.pidfile_timeout)):
+    app.run()
